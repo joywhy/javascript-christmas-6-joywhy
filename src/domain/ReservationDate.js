8fb68d174be920e4811611516d29d6593a432e73
Eunhee <82435813+joywhy@users.noreply.github.com>
2023-11-15 03:33:22 +0900

    chore(ReservationDate): 불필요한 문법 제거

--- src/domain/ReservationDate.js
+++ src/domain/ReservationDate.js
@@ -4,56 +4,45 @@ import InputError from '../error/InputError.js';
 
 class ReservationDate {
   #date;
-  #specialDay;
+  #specialDay = [3, 10, 17, 24, 25, 31];
 
   constructor(day) {
     this.#validate(day);
     this.setDate(day);
-    this.#specialDay = [3, 10, 17, 24, 25, 31];
   }
   #validate(day) {
-    if (!Validator.isInteger(Number(day))) {
-      throw new InputError(ERROR_MESSAGES.notRange);
-    }
+    if (!Validator.isInteger(Number(day))) throw new InputError(ERROR_MESSAGES.notRange);
 
-    if (!Validator.isRange(DATE_RANGE.to, DATE_RANGE.from, Number(day))) {
+    if (!Validator.isRange(DATE_RANGE.to, DATE_RANGE.from, Number(day)))
       throw new InputError(ERROR_MESSAGES.notRange);
-    }
   }
 
   setDate(date) {
     this.#date = new Date(`2023-12-${date}`);
   }
-  getDate() {
-    return this.#date.getDate();
-  }
+
   getbenefitDetails() {
     const benefitDetails = [];
 
-    if (this.isWeekend()) {
-      benefitDetails.push('weekend');
-    }
-    if (this.isChristmasDday()) {
-      benefitDetails.push('christmasDday');
-    }
-    if (this.isSpecialDay()) {
-      benefitDetails.push('specialDay');
-    }
-    if (!benefitDetails.includes('weekend')) {
-      benefitDetails.push('weekday');
-    }
+    if (this.#isWeekend()) benefitDetails.push('weekend');
+    if (this.#isChristmasDday()) benefitDetails.push('christmasDday');
+    if (this.#isSpecialDay()) benefitDetails.push('specialDay');
+    if (!benefitDetails.includes('weekend')) benefitDetails.push('weekday');
+
     return benefitDetails;
   }
-
-  isWeekend() {
+  #isWeekend() {
     const day = this.#date.getDay();
     return day === WEEKEND.friday || day === WEEKEND.saturday; //5= 금 6= 토 , 일=0
   }
-  isChristmasDday() {
-    return this.getDate() > CHRISTMAS ? false : true;
+  #isChristmasDday() {
+    return this.getDate() <= CHRISTMAS;
   }
-  isSpecialDay() {
+  #isSpecialDay() {
     return this.#specialDay.includes(this.getDate());
+  }
+  getDate() {
+    return this.#date.getDate();
   }
 }
 export default ReservationDate;
